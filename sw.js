4830cfea71b7e52a21233d4b66df749f2abdea7f
owl121226-dev <owl121226@gmail.com>
2026-09-09 23:36:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mindfulness-flat-v5';
+const CACHE='mindfulness-flat-v6';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
