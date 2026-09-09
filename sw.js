c321ea80be8e1b99bfe5fae2900f3d53b6858880
owl121226-dev <owl121226@gmail.com>
2026-09-09 23:27:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-const CACHE='palouse-flat-v2';
+const CACHE='mindfulness-flat-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return resp;
-  }).catch(()=>caches.match('./index.html'))));
+  e.respondWith(
+    fetch(e.request).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+  );
 });
